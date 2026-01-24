a0168603e39f64a1e0a642c25deda4bedb6014f2
pablo <pjestradac@gmail.com>
2026-01-24 06:03:44 -0600

    feat: sprint 1 auth service setup and signup route

---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,68 @@
+use uuid::Uuid;
+use auth_service::Application;
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0").await.expect("Failed to build application service");
+        let address = format!("http://{}", app.address.clone());
+
+        // Run the auth service in a separate async task to avoid blocking the main thread.
+        #[allow(clippy::let_underscore_future)]
+        let _ = tokio::spawn(app.run());
+
+        let http_client = reqwest::Client::new();
+
+        Self {
+            address,
+            http_client,
+        }
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client.get(&format!("{}/", self.address)).send().await.unwrap()
+    }
+
+
+
+    pub async fn post_signup<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
+        self.http_client
+            .post(&format!("{}/signup", &self.address))
+            .json(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+    pub async fn signup(&self) -> reqwest::Response {
+        self.http_client.post(&format!("{}/signup", self.address)).send().await.unwrap()
+    }
+
+    pub async fn login(&self) -> reqwest::Response {
+        self.http_client.post(&format!("{}/login", self.address)).send().await.unwrap()
+    }
+
+    pub async fn verify_2fa(&self) -> reqwest::Response {
+        self.http_client.post(&format!("{}/verify-2fa", self.address)).send().await.unwrap()
+    }
+
+    pub async fn logout(&self) -> reqwest::Response {
+        self.http_client.post(&format!("{}/logout", self.address)).send().await.unwrap()
+    }
+
+    pub async fn verify_token(&self) -> reqwest::Response {
+        self.http_client.post(&format!("{}/verify-token", self.address)).send().await.unwrap()
+    }
+
+
+}
+
+pub  fn get_random_email() -> String {
+    format!("{}@example.com", Uuid::new_v4())
+}
